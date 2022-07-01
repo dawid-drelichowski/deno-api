4d472ed745497872d02a5242168c89fdbf4b893f
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2022-07-01 11:20:03 +0200

    Exports env variables from .env

--- src/libraries.ts
+++ src/libraries.ts
@@ -2,4 +2,6 @@ export { Application, Router } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 export { Client } from 'https://deno.land/x/mysql@v2.10.2/mod.ts';
 import { config as dotenv } from 'https://deno.land/std@0.144.0/dotenv/mod.ts';
 
-export const config = await dotenv();
+export const config = await dotenv({
+  export: true
+});
